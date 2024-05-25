8502d35f1db334d6350518f9b5ec98416295bc97
Amine El Hanafi <aminecoding2023@gmail.com>
2024-05-25 14:37:00 +0100

    initializing spring boot application

---
+++ src/main/java/com/ensah/proctorsync/ProctorsyncApplication.java
@@ -0,0 +1,13 @@
+package com.ensah.proctorsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProctorsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProctorsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensah/proctorsync/ProctorsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensah.proctorsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProctorsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
