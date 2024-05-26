500946d856cf829fc367b585eba6692cff51992e
Abdessamad Abidar <abdessamad.abidar@hotmail.com>
2024-05-26 02:22:29 +0100

    add user entity for jwt token test

---
+++ src/main/java/com/ensah/proctorsync/entities/User.java
@@ -0,0 +1,64 @@
+package com.ensah.proctorsync.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+@NoArgsConstructor @AllArgsConstructor @ToString
+public class User implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
